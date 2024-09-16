85f2a8187916db0c0b8c2389fe1c635b6824ba25
artalon <felix.pinty@gmail.com>
2024-09-16 11:15:48 +0200

    feature: calculate basic scores when no strikes or spare

--- specs/bowling.spec.ts
+++ specs/bowling.spec.ts
@@ -1,4 +1,9 @@
+import { bowlingScore  } from "../src/bowling";
+
 describe('bowling', () => {
 
-    
+   it("should calculate basic scores when no strikes or spare", () => {
+    const rolls = [2,3];
+    expect(bowlingScore(rolls)).toEqual(5);
+   } );
 });
---
+++ src/bowling.ts
@@ -0,0 +1,3 @@
+export function bowlingScore(rolls: number[]): number {
+    return rolls[0] + rolls[1];
+}
